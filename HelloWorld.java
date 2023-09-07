18f20dd46e8937525516a99596857c4d76f296b3
Dmytro Karpenko <dmytro.karpenko@mrjaffesclass.com>
2023-09-07 11:05:56 -0700

    Files organizing

---
+++ HelloWorld.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class HelloWorld here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class HelloWorld
+{
+
+    public void run()
+    {
+         System.out.println("HelloWorld");
+    }
+
+    public static void main (String[] args)
+    {
+        HelloWorld sp = new HelloWorld();
+        sp.run();
+    }
+}
